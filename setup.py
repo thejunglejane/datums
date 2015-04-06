4adf4334684d171bd845a5585f02fb732578d3e8
Jane Adams <jane@thejunglejane.com>
2015-04-06 18:55:53 -0400

    Initial commit of setup script.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+from datums import __version__
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name = 'datums',
+    packages = ['datums', 'models'],
+    scripts = ['bin/datums']
+    version = __version__,
+    description = 'A PostgreSQL pipeline for Reporter data.',
+    author = 'Jane Stewart Adams',
+    author_email = 'jane@thejunglejane.com',
+    install_requires = ['sqlalchemy']
+)
