0995d70a41adf1962f1cfb6d050613329f9988ac
Jane Adams <jane@thejunglejane.com>
2015-04-07 23:03:59 -0400

    Initial commit of update and delete pipeline submodules.

--- datums/pipeline/delete.py
+++ datums/pipeline/delete.py
@@ -1,4 +1,37 @@
 from .. import models
 
+
+def delete_question(question):
+    pass
+
+
+def delete_snapshot(snapshot):
+    pass
+
+
+def delete_audio_snapshot(snapshot):
+    pass
+
+
+def delete_location_snapshot(snapshot):
+    pass
+
+
+def delete_placemark_snapshot(snapshot):
+    pass
+
+
+def delete_weather_snapshot(snapshot):
+    pass
+
+
+def delete_resposne(response, snapshot):
+    pass
+
+
 def delete_report(report):
+    pass
+
+
+def bulk_delete_reports(report):
     pass
--- datums/pipeline/update.py
+++ datums/pipeline/update.py
@@ -1,4 +1,37 @@
 from .. import models
 
+
+def update_question(question):
+    pass
+
+
+def update_snapshot(snapshot):
+    pass
+
+
+def update_audio_snapshot(snapshot):
+    pass
+
+
+def update_location_snapshot(snapshot):
+    pass
+
+
+def update_placemark_snapshot(snapshot):
+    pass
+
+
+def update_weather_snapshot(snapshot):
+    pass
+
+
+def update_resposne(response, snapshot):
+    pass
+
+
 def update_report(report):
+    pass
+
+
+def bulk_update_reports(report):
     pass
