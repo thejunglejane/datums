043e202ba06b9ab63c74e60086aa92c8e8bc6a58
Jane Adams <jane@thejunglejane.com>
2016-01-11 08:21:53 -0500

    WIP: adding tests for models.base classes.

--- datums/models/base.py
+++ datums/models/base.py
@@ -17,6 +17,34 @@ engine = create_engine(os.environ['DATABASE_URI'])
 session.configure(bind=engine)
 
 
+def database_setup(engine):
+    '''Set up the database.
+    '''
+    metadata.create_all(engine)
+
+
+def database_teardown(engine):
+    '''BURN IT ALL DOWN (╯°□°）╯︵ ┻━┻
+    '''
+    metadata.drop_all(engine)
+
+
+def _validate_and_commit(obj, action, **kwargs):
+    '''Adds/deletes the instance obj to/from the session based on the action.
+    Validates the values of the attributes in **kwargs against the attributes
+    of the instance. If all values are validated, the session will commit,
+    otherwise it will rollback.
+    '''
+    action(obj)
+    try:
+        for key, value in kwargs.iteritems():
+            assert getattr(obj, key) == value
+    except (AttributeError, AssertionError), e:
+        session.rollback()
+        raise e
+    session.commit()
+
+
 class GhostBase(Base):
 
     '''The GhostBase class extends the declarative Base class.'''
@@ -24,31 +52,14 @@ class GhostBase(Base):
     __abstract__ = True
 
     def __str__(self, attrs):
-        return '''<{0}({1})>'''.format(self.__class__.__name__, ', '.join(
-            ['='.join([attr, str(getattr(self, attr, ''))]) for attr in attrs]))
+        return '''<{0}({1})>'''.format(self.__class__.__name__, ', '.join([
+            '='.join([attr, str(getattr(self, attr, ''))]) for attr in attrs]))
 
     @classmethod
     def _get_instance(cls, **kwargs):
         '''Returns the first instance of cls with attributes matching **kwargs.
         '''
         return session.query(cls).filter_by(**kwargs).first()
-
-    @staticmethod
-    def _validate_and_commit(obj, action, **kwargs):
-        '''Adds/deletes the instance obj to/from the session based on the
-        action. Validates the values of the attributes in **kwargs against the
-        attributes of the instance. If all values are validated, the session
-        will commit, otherwise it will rollback.
-        '''
-        action(obj)
-        try:
-            for key, value in kwargs.iteritems():
-                assert getattr(obj, key) == value
-        except AssertionError:
-            session.rollback()
-            raise AssertionError
-        else:
-            session.commit()
 
     @classmethod
     def get_or_create(cls, **kwargs):
@@ -60,7 +71,7 @@ class GhostBase(Base):
         if q:
             return q
         q = cls(**kwargs)
-        cls._validate_and_commit(q, session.add, **kwargs)
+        _validate_and_commit(q, session.add, **kwargs)
         return q
 
     @classmethod
@@ -74,7 +85,7 @@ class GhostBase(Base):
         if q:
             for k in snapshot:
                 setattr(q, k, snapshot[k])
-            cls._validate_and_commit(q, session.add, **snapshot)
+            _validate_and_commit(q, session.add, **snapshot)
         else:
             cls.get_or_create(**kwargs)
 
@@ -85,7 +96,7 @@ class GhostBase(Base):
         '''
         q = cls._get_instance(**kwargs)
         if q:
-            cls._validate_and_commit(q, session.delete, **kwargs)
+            _validate_and_commit(q, session.delete, **kwargs)
 
 
 class ResponseClassLegacyAccessor(object):
@@ -95,14 +106,27 @@ class ResponseClassLegacyAccessor(object):
         self.column = column
         self.accessor = accessor
 
+    @staticmethod
+    def _confirm_or_add_response(self, obj, attr, response, action, **kwargs):
+        try:
+            getattr(obj, attr)
+        except AttributeError:
+            setattr(obj, attr, self.accessor(response))
+            self._update_validate_and_commit(
+                obj, attr, response, action, **kwargs)
+
+    def _update_validate_and_commit(
+            self, obj, attr, response, action, **kwargs):
+        '''Update the **kwargs argument before validating and committing/rolling
+        back the result.
+        '''
+        kwargs.update({attr: self.accessor(response)})
+        _validate_and_commit(obj, action, **kwargs)
+
     def get_or_create_from_legacy_response(self, response, **kwargs):
-        response_cls = self.response_class(**kwargs)
-        # Return the existing or newly created response record
-        response_cls = response_cls.get_or_create(**kwargs)
-        # If the record does not have a response, add it
-        if not getattr(response_cls, self.column):
-            setattr(response_cls, self.column, self.accessor(response))
-            cls._validate_and_commit(response_cls, session.add, **kwargs)
+        response_cls = self.response_class(**kwargs).get_or_create(**kwargs)
+        self._confirm_or_add_response(
+            response_cls, self.column, response, session.add, **kwargs)
 
     def update(self, response, **kwargs):
         response_cls = self.response_class(**kwargs)
@@ -110,8 +134,8 @@ class ResponseClassLegacyAccessor(object):
         response_cls = session.query(
             response_cls.__class__).filter_by(**kwargs).first()
         if response_cls:
-            setattr(response_cls, self.column, self.accessor(response))
-            cls._validate_and_commit(response_cls, session.add, **kwargs)
+            self._confirm_or_add_response(
+                response_cls, self.column, response, session.add, **kwargs)
         else:
             response_cls.get_or_create_from_legacy_response(response, **kwargs)
 
@@ -121,33 +145,35 @@ class ResponseClassLegacyAccessor(object):
         response_cls = session.query(
             response_cls.__class__).filter_by(**kwargs).first()
         if response_cls:
-            cls._validate_and_commit(response_cls, session.delete, **kwargs)
+            _validate_and_commit(response_cls, session.delete, **kwargs)
 
 
 class LocationResponseClassLegacyAccessor(ResponseClassLegacyAccessor):
 
     def __init__(
-            self, response_class, column, accessor, venue_column, venue_accessor):
+            self, response_class, column,
+            accessor, venue_column, venue_accessor):
         super(LocationResponseClassLegacyAccessor, self).__init__(
             response_class, column, accessor)
         self.venue_column = venue_column
         self.venue_accessor = venue_accessor
 
     def get_or_create_from_legacy_response(self, response, **kwargs):
-        ResponseClassLegacyAccessor.get_or_create_from_legacy_response(
-            self, response, **kwargs)
-        response_cls = self.response_class(**kwargs)
-        # Return the existing or newly created response record
-        response_cls = response_cls.get_or_create(**kwargs)
-        # If the record does not have a response, add it
-        if not getattr(response_cls, self.column):
+        response_cls = self.response_class(**kwargs).get_or_create(**kwargs)
+        # If the record does not have responses, add them
+        try:
+            getattr(response_cls, self.column)
+        except AttributeError:
             setattr(response_cls, self.column, self.accessor(response))
-        if not getattr(response_cls, self.venue_column):
+            kwargs.update({self.column: self.accessor(response)})
+            self._validate_and_commit(response_cls, session.add, **kwargs)
+        try:
+            getattr(response_cls, self.venue_column)
+        except AttributeError:
             setattr(
                 response_cls, self.venue_column, self.venue_accessor(response))
-        kwargs.update({self.column: self.accessor(response),
-                       self.venue_column: self.venue_accessor(response)})
-        cls._validate_and_commit(response_cls, session.add, **kwargs)
+            kwargs.update({self.venue_column: self.venue_accessor(response)})
+            self._validate_and_commit(response_cls, session.add, **kwargs)
 
     def update(self, response, **kwargs):
         response_cls = self.response_class(**kwargs)
@@ -160,16 +186,4 @@ class LocationResponseClassLegacyAccessor(ResponseClassLegacyAccessor):
                 response_cls, self.venue_column, self.venue_accessor(response))
             kwargs.update({self.column: self.accessor(response),
                            self.venue_column: self.venue_accessor(response)})
-            cls._validate_and_commit(response_cls, session.add, **kwargs)
-
-
-def database_setup(engine):
-    '''Set up the database.
-    '''
-    metadata.create_all(engine)
-
-
-def database_teardown(engine):
-    '''BURN IT ALL DOWN (╯°□°）╯︵ ┻━┻
-    '''
-    metadata.drop_all(engine)
+            self._validate_and_commit(response_cls, session.add, **kwargs)
--- tests/models.py
+++ tests/models.py
@@ -3,7 +3,58 @@
 from datums import models
 from sqlalchemy.orm import query
 import mock
+import random
 import unittest
+
+
+class TestModelsBase(unittest.TestCase):
+
+    def setUp(self):
+        self.GhostBaseInstance = models.base.GhostBase()
+
+    def tearDown(self):
+        del self.GhostBaseInstance
+
+    @mock.patch.object(models.base.metadata, 'create_all')
+    def test_database_setup(self, mock_create_all):
+        models.base.database_setup(models.engine)
+        mock_create_all.assert_called_once_with(models.engine)
+
+    @mock.patch.object(models.base.metadata, 'drop_all')
+    def test_database_teardown(self, mock_drop_all):
+        models.base.database_teardown(models.engine)
+        mock_drop_all.assert_called_once_with(models.engine)
+
+    @mock.patch.object(models.session, 'commit')
+    @mock.patch.object(models.session, 'add')
+    def test_validate_and_commit_valid_kwargs(
+            self, mock_session_add, mock_session_commit):
+        '''Does the _validate_and_commit() method commit the session if the
+        kwargs are valid?
+        '''
+        kwargs = {'section_identifier': 'bar'}
+        obj = models.Snapshot(**kwargs)
+        setattr(self.GhostBaseInstance, 'section_identifier', 'bar')
+        models.base._validate_and_commit(obj, mock_session_add, **kwargs)
+        mock_session_add.assert_called_once_with(obj)
+        self.assertTrue(mock_session_commit.called)
+        delattr(self.GhostBaseInstance, 'section_identifier')
+    
+    @mock.patch.object(models.session, 'rollback')
+    @mock.patch.object(models.session, 'add')
+    def test_validate_and_commit_invalid_kwargs(
+            self, mock_session_add, mock_session_rollback):
+        '''Does the _validate_and_commit() method rollback the session if the
+        kwargs are invalid and raise an AssertionError?
+        '''
+        kwargs = {'section_identifier': 'bar'}
+        obj = models.Snapshot(**{'section_identifier': 'baz'})
+        setattr(self.GhostBaseInstance, 'section_identifier', 'bar')
+        with self.assertRaises(AssertionError):
+            models.base._validate_and_commit(obj, mock_session_add, **kwargs)
+        mock_session_add.assert_called_once_with(obj)
+        self.assertTrue(mock_session_rollback.called)
+        delattr(self.GhostBaseInstance, 'section_identifier')
 
 
 @mock.patch.object(query.Query, 'first')
@@ -43,41 +94,6 @@ class TestGhostBase(unittest.TestCase):
         mock_session_query.assert_called_once_with(models.base.GhostBase)
         mock_query_filter_by.assert_called_once_with(**{'foo': 'bar'})
         self.assertTrue(mock_query_first.called)
-
-    @mock.patch.object(models.session, 'commit')
-    @mock.patch.object(models.session, 'add')
-    def test_validate_and_commit_valid_kwargs(
-            self, mock_session_add, mock_session_commit,
-            mock_session_query, mock_query_filter_by, mock_query_first):
-        '''Does the _validate_and_commit() method commit the session if the
-        **kwargs are valid?
-        '''
-        kwargs = {'foo': 'bar'}
-        obj = models.Snapshot(**kwargs)
-        setattr(self.GhostBaseInstance, 'foo', 'bar')
-        self.GhostBaseInstance._validate_and_commit(
-            obj, mock_session_add, **kwargs)
-        mock_session_add.assert_called_once_with(obj)
-        self.assertTrue(mock_session_commit.called)
-        delattr(self.GhostBaseInstance, 'foo')
-
-    @mock.patch.object(models.session, 'rollback')
-    @mock.patch.object(models.session, 'add')
-    def test_validate_and_commit_invalid_kwargs(
-            self, mock_session_add, mock_session_rollback,
-            mock_session_query, mock_query_filter_by, mock_query_first):
-        '''Does the _validate_and_commit() method rollback the session if the
-        **kwargs are invalid and raise an AssertionError?
-        '''
-        kwargs = {'foo': 'bar'}
-        obj = models.Snapshot(**{'foo': 'baz'})
-        setattr(self.GhostBaseInstance, 'foo', 'bar')
-        with self.assertRaises(AssertionError):
-            self.GhostBaseInstance._validate_and_commit(
-                obj, mock_session_add, **kwargs)
-        mock_session_add.assert_called_once_with(obj)
-        self.assertTrue(mock_session_rollback.called)
-        delattr(self.GhostBaseInstance, 'foo')
 
     @mock.patch.object(models.session, 'add')
     def test_get_or_create_get(self, mock_session_add, mock_session_query,
@@ -120,34 +136,121 @@ class TestGhostBase(unittest.TestCase):
         self.GhostBaseInstance.update(snapshot={'foo': 'bar'})
         self.assertTrue(mock_session_add.called)
 
+    @mock.patch.object(models.base, '_validate_and_commit')
     @mock.patch.object(models.session, 'delete')
-    def test_delete_exists(self, mock_session_delete, mock_session_query,
-                           mock_query_filter_by, mock_query_first):
-        '''Does the delete() method delete an existing instance of the class
-        from the session?
+    def test_delete_exists(
+            self, mock_session_delete, mock_validate_and_commit,
+            mock_session_query, mock_query_filter_by, mock_query_first):
+        '''Does the delete() method validate an existing instance of the class
+        before deleting from the session?
         '''
         mock_query_first.return_value = True
         self.GhostBaseInstance.delete()
-        self.assertTrue(mock_session_delete.called)
+        self.assertTrue(mock_validate_and_commit.called)
 
+    @mock.patch.object(models.base, '_validate_and_commit')
     @mock.patch.object(models.session, 'delete')
     def test_delete_does_not_exist(
-            self, mock_session_delete, mock_session_query,
-            mock_query_filter_by, mock_query_first):
+            self, mock_session_delete, mock_validate_and_commit,
+            mock_session_query, mock_query_filter_by, mock_query_first):
         '''Does the delete() method do nothing if the instance does not already
         exists?
         '''
         mock_query_first.return_value = None
         self.GhostBaseInstance.delete()
-        mock_session_delete.assert_not_called()
-class TestModelsBase(unittest.TestCase):
+        mock_validate_and_commit.assert_not_called()
 
-    @mock.patch.object(models.base.metadata, 'create_all')
-    def test_database_setup(self, mock_create_all):
-        models.base.database_setup(models.engine)
-        mock_create_all.assert_called_once_with(models.engine)
 
-    @mock.patch.object(models.base.metadata, 'drop_all')
-    def test_database_teardown(self, mock_drop_all):
-        models.base.database_teardown(models.engine)
-        mock_drop_all.assert_called_once_with(models.engine)
+class TestResponseClassLegacyAccessor(unittest.TestCase):
+
+    _response_classes = models.Response.__subclasses__()
+    _response_classes.remove(models.LocationResponse)  # tested separately
+
+    def setUp(self, mock_response=random.choice(_response_classes)):
+        self.LegacyInstance = models.base.ResponseClassLegacyAccessor(
+            response_class=mock_response, column='foo_response',
+            accessor=(lambda x: x.get('foo')))
+        self.test_response = {'foo': 'bar'}
+
+    def tearDown(self):
+        del self.LegacyInstance
+
+    # TODO (jsa): test ResponseClassLegacyAccessor.update() method separately
+    @mock.patch.object(models.base, '_validate_and_commit')
+    @mock.patch.object(models.session, 'add')
+    def test_update_validate_and_commit_update(
+            self, mock_session_add):
+        '''Does the _update_validate_and_commit() method update the kwargs
+        dictionary and validate the kwargs?
+        '''
+        obj = self.LegacyInstance.response_class
+        response = {'foo': 'bar'}
+        kw = {}
+        self.LegacyInstance._update_validate_and_commit(
+            obj, self.LegacyInstance.column,
+            response, models.session.add, **kw)
+        self.assertTrue(hasattr(kw, 'foo_response'))
+        mock_validate_and_commit.assert_called_once_with(
+            obj, models.session.add, **kw)
+
+    def test_confirm_or_add_response_attr_present(self):
+        pass
+
+    def test_confirm_or_add_response_attr_not_present(self):
+        pass
+
+    # @mock.patch.object(
+    #     models.base.ResponseClassLegacyAccessor, '_update_validate_and_commit')
+    # @mock.patch.object(models.base.GhostBase, 'get_or_create')
+    # @mock.patch.object(models.session, 'add')
+    # def test_get_or_create_from_legacy_response_exists(
+    #         self, mock_session_add, mock_get_or_create,
+    #         mock_validate_and_commit):
+    #     '''Does the get_or_create_from_legacy_response() method call the
+    #     get_or_create() method and do nothing if the instance exists?
+    #     '''
+    #     mock_get_or_create.return_value = self.LegacyInstance.response_class()
+    #     self.LegacyInstance.get_or_create_from_legacy_response(
+    #         self.test_response)
+    #     self.assertTrue(mock_get_or_create.called)
+    #     mock_validate_and_commit.assert_not_called()
+
+    # @mock.patch.object(
+    #     models.base.ResponseClassLegacyAccessor, '_validate_and_commit')
+    # @mock.patch.object(models.base.GhostBase, 'get_or_create')
+    # @mock.patch.object(models.session, 'query', return_value=query.Query(
+    #     models.Snapshot))
+    # @mock.patch.object(models.session, 'add')
+    # def test_get_or_create_from_legacy_response_does_not_exist(
+    #         self, mock_session_add, mock_session_query,
+    #         mock_get_or_create, mock_validate_and_commit):
+    #     '''Does the get_or_create_from_legacy_response() method call the
+    #     get_or_create() method, set the attribute, and validate it if the
+    #     instance does not exist?
+    #     '''
+    #     t_obj = self.LegacyInstance.response_class()
+    #     mock_get_or_create.return_value = t_obj
+    #     with self.assertRaises(AssertionError):
+    #         self.LegacyInstance.get_or_create_from_legacy_response(
+    #             self.test_response)
+    #     self.assertTrue(mock_get_or_create.called)
+    #     mock_validate_and_commit.assert_called_once_with(
+    #         t_obj, mock_session_add)
+
+    def test_update(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+
+# class TestLocationResponseClassLegacyAccessor(unittest.TestCase):
+
+#     @mock.patch('models.LocationResponse')
+#     def setUp(self, mock_response):
+#         self.LegacyInstance = models.base.LocationResponseClassLegacyAccessor(
+#             response_class=mock_response, column='foo',
+#             accessor=(lambda x: [i for i in x.get('tokens', [])]))
+
+#     def tearDown(self):
+#         del self.LegacyInstance
