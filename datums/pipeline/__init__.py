f3de4ad160a6179a0aca6739985dc0ed9705083e
Jane Adams <jane@thejunglejane.com>
2016-01-20 17:46:01 -0500

    WIP objectify pipeline.

--- datums/pipeline/__init__.py
+++ datums/pipeline/__init__.py
@@ -7,140 +7,151 @@ import uuid
 import warnings
 from datums import models
 
-__all__ = ['codec', 'mapers']
+__all__ = ['codec', 'mappers']
 
 
-def _prepare_snapshot(snapshot):
-    report = snapshot.copy()
-    responses = report.pop('responses', None)
-    photoset = report.pop('photoSet', None)  # TODO (jsa): add support
-    return report, responses, photoset
+def _traverse_report():
+    pass
 
 
-def _question(question, action):
-    '''Perform the action specified (models.Question.get_or_create,
-    models.Question.update, or models.Question.delete) on the question.
-    '''
-    question_dict = {'type': question['questionType'],
-                     'prompt': question['prompt']}
-    action(**question_dict)
+class QuestionPipeline(object):
+
+    def __init__(self, question):
+        self.question = question
+
+        self.question_dict = {'type': self.question['questionType'],
+                              'prompt': self.question['prompt']}
+
+    def add(self):
+        models.Question.get_or_create(**self.question_dict)
+
+    def update(self):
+        models.Question.update(**self.question_dict)
+
+    def delete(self):
+        models.Question.delete(**self.question_dict)
 
 
-def _response(response, action, **kwargs):
-    '''Perform the action specified
-    (models.Response.get_or_create_from_legacy_response, models.Response.update,
-    or models.Response.delete) on the response.
-    '''
-    action(response, **kwargs)
+class ResponsePipeline(object):
+
+    def __init__(self, response, report):
+        self.response = response
+        self.report = report
+
+        self.accessor, self.ids = codec.get_response_accessor(
+            self.response, self.report)
+
+    def add(self):
+        self.accessor.get_or_create_from_legacy_response(
+            self.response, **self.ids)
+
+    def update(self):
+        self.accessor.update(self.response, **self.ids)
+
+    def delete(self):
+        self.accessor.delete(self.response, **self.ids)
 
 
-def _traverse_report(report, action, key_mapper=mappers._report_key_mapper):
-    '''Return the dictionary of **kwargs based on the report and the action
-    specified.
-    '''
-    _top_level = [key for key in report if not isinstance(report[key], dict)]
-    # Set aside nested reports for recursion
-    _nested_level = [key for key in report if isinstance(report[key], dict)]
-    top_level_dict = {}
-    nested_level_dict = {}
-    for key in _top_level:
-        try:
-            item = mappers._key_type_mapper[key](
-                str(report[key]) if key != 'draft' else report[key])
-        except KeyError:
-            item = report[key]
-        finally:
-            top_level_dict[key_mapper[key]] = item
-    for key in _nested_level:
-        try:
-            nested_level_dict[key] = mappers._key_type_mapper[key](
-                str(report[key]) if key != 'draft' else report[key])
-        except KeyError:
-            nested_level_dict[key] = report[key]
-        nested_level_dict[key][
-            'reportUniqueIdentifier'] = mappers._key_type_mapper[
-                'uniqueIdentifier'](report['uniqueIdentifier'])
-        if key == 'placemark':
-            # Add the parent location report UUID
-            nested_level_dict[key][
-                'locationUniqueIdentifier'] = nested_level_dict[key].pop(
-                    'reportUniqueIdentifier')
-        elif key == 'altitude':
+class ReportPipeline(object):
+
+    def __init__(self, report):
+        self.report = report
+
+    def _report(self, action, key_mapper=mappers._report_key_mapper):
+        '''Return the dictionary of **kwargs with the correct datums attribute
+        names and data types for the top level of the report, and return the
+        nested levels separately.
+        '''
+        _top_level = [
+            k for k, v in self.report.items() if not isinstance(v, dict)]
+        _nested_level = [
+            k for k, v in self.report.items() if isinstance(v, dict)]
+        top_level_dict = {}
+        nested_levels_dict = {}
+        for key in _top_level:
             try:
-                nested_level_dict[key]['uniqueIdentifier'] = report[key][
-                    'uniqueIdentifier']
-            except KeyError:  # not all altitude reports have a UUID
-                if action == 'get_or_create':
-                    nested_level_dict[key]['uniqueIdentifier'] = uuid.uuid4()
+                item = mappers._key_type_mapper[key](str(
+                    self.report[key]) if key != 'draft' else self.report[key])
+            except KeyError:
+                item = self.report[key]
+            finally:
+                top_level_dict[key_mapper[key]] = item
+        for key in _nested_level:
+            nested_levels_dict[key] = self.report[key]
+            # Add the parent report ID
+            nested_levels_dict[key][
+                'reportUniqueIdentifier'] = mappers._key_type_mapper[
+                    'uniqueIdentifier'](str(self.report['uniqueIdentifier']))
+            if key == 'placemark':
+                # Add the parent location report UUID
+                nested_levels_dict[key][
+                    'locationUniqueIdentifier'] = nested_levels_dict[key].pop(
+                        'reportUniqueIdentifier')
+            # Create UUID for altitude report if there is not one and the action
+            # is get_or_create, else delete the altitude report from the nested
+            # levels and warn that it will not be updated
+            if 'uniqueIdentifier' not in nested_levels_dict[key]:
+                if action.__func__.func_name == 'get_or_create':
+                    nested_levels_dict[key]['uniqueIdentifier'] = uuid.uuid4()
                 else:
+                    del nested_levels_dict[key]
                     warnings.warn('''
                         No uniqueIdentifier found for AltitudeReport in {0}.
                         Existing altitude report will not be updated.
-                        '''.format(report['uniqueIdentifier']))
-                    del nested_level_dict[key]
-    return top_level_dict, nested_level_dict
+                        '''.format(self.report['uniqueIdentifier']))
+        return top_level_dict, nested_levels_dict
+
+    def add(self, action=models.Report.get_or_create,
+            key_mapper=mappers._report_key_mapper):
+        top_level, nested_levels = self._report(action, key_mapper)
+        action(**top_level)
+        for nested_level in nested_levels:
+            try:
+                key_mapper = mappers._report_key_mapper[nested_level]
+            except KeyError:
+                key_mapper = mappers._report_key_mapper[
+                    'location'][nested_level]
+            ReportPipeline(nested_levels[nested_level]).add(
+                mappers._model_type_mapper[
+                    nested_level].get_or_create, key_mapper)
+
+    def update(self, action=models.Report.update,
+               key_mapper=mappers._report_key_mapper):
+        top_level, nested_levels = self._report(action, key_mapper)
+        action(**top_level)
+        for nested_level in nested_levels:
+            try:
+                key_mapper = mappers._report_key_mapper[nested_level]
+            except KeyError:
+                key_mapper = mappers._report_key_mapper[
+                    'location'][nested_level]
+            ReportPipeline(nested_levels[nested_level]).update(
+                mappers._model_type_mapper[nested_level].update, key_mapper)
+
+    def delete(self):
+        models.Report.delete(**{'id': mappers._key_type_mapper[
+            'uniqueIdentifier'](str(self.report['uniqueIdentifier']))})
 
 
-def _report_add(
-        report, type=models.Report, key_mapper=mappers._report_key_mapper):
-    top_level_report, nested_report = _traverse_report(
-        report, 'get_or_create', key_mapper)
-    type.get_or_create(**top_level_report)
-    for key in nested_report:
-        try:
-            nested_key_mapper = mappers._report_key_mapper[key]
-        except KeyError:
-            nested_key_mapper = mappers._report_key_mapper['location'][key]
-        _report_add(
-            nested_report[key], mappers._model_type_mapper[key], nested_key_mapper)
+class SnapshotPipeline(object):
 
+    def __init__(self, snapshot):
+        self.snapshot = snapshot
 
-def _report_update(
-        report, type=models.Report, key_mapper=mappers._report_key_mapper):
-    top_level_report, nested_report = _traverse_report(
-        report, 'update', key_mapper)
-    type.update(**top_level_report)
-    for key in nested_report:
-        try:
-            nested_key_mapper = mappers._report_key_mapper[key]
-        except KeyError:
-            nested_key_mapper = mappers._report_key_mapper['location'][key]
-        _report_update(
-            nested_report[key], mappers._model_type_mapper[key], nested_key_mapper)
+        self.report = self.snapshot.copy()
+        self.responses = self.report.pop('responses')
 
+        _ = self.report.pop('photoSet', None)  # TODO (jsa): add support
 
-def add_question(question):
-    _question(question, models.Question.get_or_create)
+    def add(self):
+        ReportPipeline(self.report).add()
+        for response in self.responses:
+            ResponsePipeline(response, self.report).add()
 
+    def update(self):
+        ReportPipeline(self.report).update()
+        for response in self.responses:
+            ResponsePipeline(response, self.report).update()
 
-def add_snapshot(snapshot):
-    report, responses, photoset = _prepare_snapshot(snapshot)
-    _report_add(report)
-    for response in responses:
-        accessor, ids = codec.get_response_accessor(response, report)
-        _response(response, accessor.get_or_create_from_legacy_response, **ids)
-
-
-def update_question(question):
-    _question(question, models.Question.update)
-
-
-def update_snapshot(snapshot):
-    report, responses, photoset = _prepare_snapshot(snapshot)
-    _report_update(report)
-    for response in responses:
-        accessor, ids = codec.get_response_accessor(response, report)
-        _response(response, accessor.update, **ids)
-
-
-def delete_question(question):
-    _question(question, models.Question.delete)
-
-
-def delete_snapshot(snapshot):
-    # Deleting a report cascades to all nested reports and associated responses,
-    # so only the top-level report needs to be deleted
-    report, responses, photoset = _prepare_snapshot(snapshot)
-    models.Report.delete(**{
-        mappers._report_key_mapper['uniqueIdentifier']: mappers._key_type_mapper[
-            'uniqueIdentifier'](str(report['uniqueIdentifier']))})
+    def delete(self):
+        ReportPipeline(self.report).delete()
